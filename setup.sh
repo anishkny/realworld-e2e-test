96372ca27eea55511ad2ec8264595f3eb6ddb4b4
akarandi <anish.karandikar@mathworks.com>
2017-09-15 11:51:45 -0400

    Add logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+## Install top level dependencies
+pwd
+yarn
+test -e node_modules/
+test -e node_modules/conduit-node/
+ls -lhta node_modules/conduit-node/
+
 ## Start app backend
 cd node_modules/conduit-node/
 yarn
