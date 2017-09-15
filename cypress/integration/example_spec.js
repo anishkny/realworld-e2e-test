ea0f6834141166acc74a1e695055f6d11399568c
akarandi <anish.karandikar@mathworks.com>
2017-09-15 13:52:22 -0400

    Add Cypress with basic test

---
+++ cypress/integration/example_spec.js
@@ -0,0 +1,6 @@
+describe('Smoke Test', function() {
+  it('should load landing page', function() {
+    cy.visit('http://localhost:4000');
+    cy.title().should('include', 'Home — Conduit');
+  });
+});
