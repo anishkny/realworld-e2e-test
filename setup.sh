2468a3622d109eb204de80fd69f90d07078403d6
Anish Karandikar <anishkny@gmail.com>
2017-09-16 18:04:51 -0400

    Pull SUT via yarn. Cache dependencies.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,22 @@
 #!/bin/bash -x
 export PROJECT_ROOT=`pwd`
+export SUT_FOLDER=`pwd`/sut
 
 ## Install system under test (SUT)
-rm -rf sut; mkdir sut; cd sut/
-git clone https://github.com/gothinkster/angularjs-realworld-example-app
-git clone https://github.com/gothinkster/node-express-realworld-example-app.git
-cd $PROJECT_ROOT
+cd $SUT_FOLDER
+yarn
 
 ## Start app backend
-cd ./sut/node-express-realworld-example-app/
+cd $SUT_FOLDER/node_modules/conduit-node/
 yarn
 yarn run start &
-cd $PROJECT_ROOT
 sleep 5
 
 ## Test backend endpoint
 curl 'http://localhost:3000/api/tags'
 
 ## Start app frontend
-cd ./sut/angularjs-realworld-example-app/
+cd $SUT_FOLDER/node_modules/conduit-angularjs/
 yarn
 patch ./src/js/config/app.constants.js $PROJECT_ROOT/app.constants.js.patch
 cat ./src/js/config/app.constants.js
@@ -27,7 +25,6 @@ rm -rf ./dist
 test -e ./dist
 yarn add http-server
 ./node_modules/.bin/http-server dist/ -a localhost -p 4000 &
-cd $PROJECT_ROOT
 sleep 5
 
 ## Test frontend endpoint
