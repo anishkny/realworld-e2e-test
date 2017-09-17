2d160dfa0f222a89c23bf425951902ce87dd6096
Anish Karandikar <anishkny@gmail.com>
2017-09-17 09:37:13 -0400

    Use React/Redux frontend, instead of AngularJS.

--- sut/start.sh
+++ sut/start.sh
@@ -10,17 +10,29 @@ sleep 5
 ## Test backend endpoint
 curl 'http://localhost:3000/api/tags'
 
-## Start app frontend
-cd $SUT_FOLDER/node_modules/conduit-angularjs/
+
+## Start AngularJS app frontend if requested
+if [[ "$START_ANGULARJS" ]]; then
+  cd $SUT_FOLDER/node_modules/conduit-angularjs/
+  yarn
+  cp $SUT_FOLDER/app.constants.js.modified ./src/js/config/app.constants.js
+  cat ./src/js/config/app.constants.js
+  rm -rf ./dist
+  ./node_modules/.bin/gulp build
+  test -e ./dist
+  yarn add http-server
+  ./node_modules/.bin/http-server dist/ -a localhost -p 4000 &
+  sleep 5
+
+  ## Test frontend endpoint
+  curl 'http://localhost:4000/'
+fi
+
+## Start React-Redux app forntend
+cd $SUT_FOLDER/node_modules/react-redux-realworld-example-app/
+patch --forward ./src/agent.js $SUT_FOLDER/agent.js.patch || true
 yarn
-cp $SUT_FOLDER/app.constants.js.modified ./src/js/config/app.constants.js
-cat ./src/js/config/app.constants.js
-rm -rf ./dist
-./node_modules/.bin/gulp build
-test -e ./dist
-yarn add http-server
-./node_modules/.bin/http-server dist/ -a localhost -p 4000 &
-sleep 5
+BROWSER=none yarn start &
 
 ## Test frontend endpoint
-curl 'http://localhost:4000/'
+curl 'http://localhost:4100/'
--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -4,17 +4,19 @@ const puppeteer = require('puppeteer');
 var browser = null;
 var page = null;
 var newUser = {
-  username: casual.username,
+  username: casual.first_name,
   email: casual.email,
   password: casual.password
 };
 
 beforeAll(async() => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
-  browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 250,
-  });
+  browser = await puppeteer.launch(
+    /* {
+        headless: false,
+        slowMo: 250,
+      } */
+  );
   page = await browser.newPage();
   await page.setViewport({
     width: 1024,
@@ -27,13 +29,18 @@ afterAll(async() => {
 });
 
 test('Landing page loads', async() => {
-  await page.goto('http://localhost:4000');
+  await page.goto('http://localhost:4100');
+  await page.waitForSelector('nav.navbar');
+  await page.waitForSelector('div.home-page');
+  await page.waitForSelector('div.home-page');
   await page.screenshot({ path: '.screenshots/landing_page.png' });
 });
 
 test('Signup', async() => {
-  await page.goto('http://localhost:4000',  {waitUntil: 'networkidle'});
-  await page.click('a[ui-sref="app.register"]');
+
+  // Register new user
+  await page.goto('http://localhost:4100');
+  await page.click('a[href*="register"]');
   await page.screenshot({ path: '.screenshots/signup_01_start.png' });
   await page.focus('input[placeholder="Username"]');
   await page.type(newUser.username);
@@ -42,17 +49,15 @@ test('Signup', async() => {
   await page.focus('input[placeholder="Password"]');
   await page.type(newUser.password);
   await page.screenshot({ path: '.screenshots/signup_02_filled.png' });
-  await page.focus('input[placeholder="Password"]');
-  await page.press('Enter');
-  // const signupButton = await page.$('button');
-  // console.log(`signupButton.innerText = [${signupButton.innerText}]`)
-  // await signupButton.click();
-  // await page.click('button[type="submit"]');
-  // var functionToWait = `document.querySelector('a[ui-sref^="app.profile.main"]').innerText == '${newUser.username}'`;
-  // console.log(`functionToWait = [${functionToWait}]`);
-  // await page.waitForFunction(functionToWait);
-  await page.waitFor(5000);
+  await page.click('button');
+
+  // Verify username is displayed in navbar right side
+  var expectedUsername = newUser.username.toLowerCase();
+  var profileLinkSelector = `a[href*="${expectedUsername}"]`;
+  await page.waitForSelector(profileLinkSelector);
   await page.screenshot({ path: '.screenshots/signup_03_done.png' });
-  await page.goto('http://localhost:4000');
-  await page.screenshot({ path: '.screenshots/signup_04_homepage.png' });
+  var profileLinkInnerText = await page.$eval(profileLinkSelector, el => el.innerText);
+  console.log(`profileLinkInnerText = [${profileLinkInnerText}]`);
+  expect(profileLinkInnerText).toBe(expectedUsername);
+
 });
