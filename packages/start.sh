50796367de1ce1b39f92c9e80eda79110a51cb0f
Anish Karandikar <anishkny@gmail.com>
2018-05-29 13:12:36 -0400

    chore(ci): Use npm instead of yarn

--- packages/start.sh
+++ packages/start.sh
@@ -6,8 +6,8 @@ export SUT_FOLDER=`pwd`
 # Start app backend
 ########################################
 cd $SUT_FOLDER/node_modules/conduit-node/
-yarn
-yarn run start &
+npm install
+npm run start &
 sleep 5
 
 # Test backend API endpoint
@@ -21,9 +21,9 @@ cd $SUT_FOLDER/node_modules/react-redux-realworld-example-app/
 
 # Make API_ROOT point to local backend
 patch --forward ./src/agent.js $SUT_FOLDER/agent.js.patch || true
-yarn
+npm install
 rm -rf ./build/
-yarn build
+npm run build
 $SUT_FOLDER/node_modules/.bin/http-server -p 4100 -a localhost ./build/ &
 sleep 5
 
